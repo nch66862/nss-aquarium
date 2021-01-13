1ab2ae001240b13be76197e22a54a0811a3c2e6f
Nick Carver <nicholascarver2@outlook.com>
2021-01-13 13:23:27 -0600

    added some data to origin and fish. Added some descriptions to fish data to make it more clear.

--- scripts/fish/fish.js
+++ scripts/fish/fish.js
@@ -2,10 +2,10 @@ export const Fish = (fish) => {
     return `
         <section class="fishCard">
             <p class="fish__name">${fish.name}</p>
-            <p class="fish__species">${fish.species}</p>
-            <p class="fish__location">${fish.location}</p>
-            <p class="fish__length">${fish.length}</p>
-            <p class="fish__diet">${fish.food}</p>
+            <p class="fish__species">Species: ${fish.species}</p>
+            <p class="fish__location">Harvested: ${fish.location}</p>
+            <p class="fish__length">Length: ${fish.length}</p>
+            <p class="fish__diet">Diet: ${fish.food}</p>
             <div><img  class="fishCard__img" src="${fish.image}" /></div>
         </section>
     `
--- scripts/tips/TipDataProvider.js
+++ scripts/tips/TipDataProvider.js
@@ -1,7 +1,19 @@
 const tipCollection = [
     {
-        theTip: "salt lake city",
-    }
+        theTip: "fish will eat until they burst if you keep feeding them",
+    },
+    {
+        theTip: "the colder the water, the better. more oxygen.",
+    },
+    {
+        theTip: "old fish have lived longer than baby fish.",
+    },
+    {
+        theTip: "draw pictures on the glass. Fish appreciate home updates too.",
+    },
+    {
+        theTip: "tuck everyone in before turning on the night light.",
+    },
 ]
 
 export const useTip = () => {
