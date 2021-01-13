a0698641d43fa4444c127618e4b9706c09210b51
Nick Carver <nicholascarver2@outlook.com>
2021-01-13 10:56:11 -0600

    removed a for loop and import statement from main.js because they werent needed anymore. First commit on this branch

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,3 @@
-import { useFish } from './fish/FishDataProvider.js'
 import { FishList } from './fish/FishList.js'
-
-const allTheFish = useFish()
-
-for (const fish of allTheFish) {
-    console.log(fish)
-}
 
 FishList ()
