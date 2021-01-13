4a47aab59162c9e57c630e342778e1508ec0e54f
Nick Carver <nicholascarver2@outlook.com>
2021-01-13 12:07:14 -0600

    I broke the code. Throwing a location error. But i am hungry and going to lunch

--- scripts/tips/Tip.js
+++ scripts/tips/Tip.js
@@ -1,10 +1,5 @@
-export const Location = (location) => {
+export const Tip = (tip) => {
     return `
-        <section class="locationCard">
-            <p class="locationCard__city">${location.city}</p>
-            <p class="locationCard__country">${location.country}</p>
-            <p class="locationCard__waterBody">${location.waterBody}</p>
-            <div><img  class="locationCard__img" src="${location.image}" /></div>
-        </section>
+        <li>${tip.theTip}</li>
     `
 }
