bb32ccdca8539678708e24c897b325ccc50ec134
Nick Carver <nicholascarver2@outlook.com>
2021-01-13 09:53:11 -0600

    successfully got the fish to import to my html

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,15 +1,27 @@
 import { useFish } from './FishDataProvider.js'
+import { Fish } from "./Fish.js"
 
 export const FishList = () => {
 
     // Get a reference to the `<article class="content">` element
-    const contentElement = document.querySelector("insert selector here")
+    const contentElement = document.querySelector(".putItHere")
     const fishes = useFish()
+
+    let fishHTMLRepresentations = ""
+    for (const fish of fishes) {
+        fishHTMLRepresentations+= Fish(fish)
+        /*
+            Invoke the Fish component function
+            and pass the current fish object as an argument.
+            Each time, add the return value to the
+            fishHTMLRepresentations variable with `+=`
+        */
+    }
 
     // Add to the existing HTML in the content element
     contentElement.innerHTML += `
-        <article class="fishList">
-            All the fish go here!
+        <article class="containerLeft__fishList contentArticle">
+            ${fishHTMLRepresentations}
         </article>
     `
 }
